edb5469a1bd26645c260ab9dc0df1ba168933948
nsinanovic <nusretasinanovic@gmail.com>
2014-05-16 22:20:39 +0200

    login i signup radi

---
+++ app/login/app.js
@@ -0,0 +1,11 @@
+angular.module( 'league', [
+    'templates-app',
+    'templates-common',
+    'league.home',
+    'league.about',
+    'league.club',
+    'league.team',
+    'league.login',
+    'ui.state',
+    'ui.route'
+])
